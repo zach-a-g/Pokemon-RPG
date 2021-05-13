f5c5c7cb69384ba34adc6c069ae9efd0798ac8b6
Keith Benjamin <keith.i.benj@gmail.com>
2021-05-13 14:35:28 -0500

    updated class

--- classes.py
+++ classes.py
@@ -1,10 +1,21 @@
+# class Pokemon:
+#     def __init__(self, name, health, attack, defense, items):
+#         self.name = name
+#         self.health = health
+#         self.attack = attack
+#         self.defense = defense
+#         self.items = items
+
+# Updated class
 class Pokemon:
-    def __init__(self, name, health, attack, defense, items):
+    def __init__(self, name, health, basic_attack, secondary_attack, special):
         self.name = name
-        self.health = health
-        self.attack = attack
-        self.defense = defense
-        self.items = items
+        self.healh = health
+        self.basic_attack = basic_attack
+        self.secondary_attack = secondary_attack
+        self.special = special
+
+
 
 class Charizard(Pokemon):
     pass
@@ -21,8 +32,16 @@ class Gyarados(Pokemon):
 class Mewtwo(Pokemon):
     pass
 
-charizard = Pokemon("Charizard", 100, 50, 25, 0)
-squirtle = Pokemon("Squirtle", 100, 50, 25, 0)
-bulbasaur = Pokemon("Bulbasaur", 100, 50, 25, 0)
-gyarados = Pokemon("Gyrados", 100, 50, 25, 0)
-mewtwo = Pokemon("Mewtwo", 100, 50, 25, 0)
+# charizard = Pokemon("Charizard", 100, 50, 25, 0)
+# squirtle = Pokemon("Squirtle", 100, 50, 25, 0)
+# bulbasaur = Pokemon("Bulbasaur", 100, 50, 25, 0)
+# gyarados = Pokemon("Gyrados", 100, 50, 25, 0)
+# mewtwo = Pokemon("Mewtwo", 100, 50, 25, 0)
+
+
+
+charizard = Pokemon("Charizard", 100, 50, 150, 110)
+squirtle = Pokemon("Squirtle", 100, 40, 60, 110)
+bulbasaur = Pokemon("Bulbasaur", 100, 45, 55, 120)
+gyarados = Pokemon("Gyarados", 100, 40, 90, 140)
+mewtwo = Pokemon("Mewtwo", 150, 60, 100, 120)
