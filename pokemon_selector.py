a362bcf7aa169c87ccb91cc502ac7b517846b203
Sarah dePalo <sarahdepalo99@gmail.com>
2021-05-13 14:28:23 -0400

    fixed pokemon selector function

--- pokemon_selector.py
+++ pokemon_selector.py
@@ -3,17 +3,45 @@ from random import randint
 
 #Basic class and subclasses for testing purposes
 
-# class Pokemon:
-    
+class Pokemon:
+    def __init__(self, name, health, basic_attack, secondary_attack, special_attack):
+        self.name = name
+        self.health = health
+        self.bounty = 0
+        self.potions = 1
+        self.basic_attack = basic_attack
+        self.secondary_attack = secondary_attack
+        self.special_attack = special_attack
+        
 
-# class Charizard(Pokemon):
+class Charizard(Pokemon):
+    def print_status(self):
+        print("%s has %d health and %d power" % (self.name, self.health, self.power))
+
+
+class Squirtle(Pokemon):
+    def print_status(self):
+        print("%s has %d health and %d power" % (self.name, self.health, self.power))
+
+class Bulbasaur(Pokemon):
+    def print_status(self):
+        print("%s has %d health and %d power" % (self.name, self.health, self.power))
+        
+
+class Gyarados(Pokemon):
+    def print_status(self):
+        print("%s has %d health and %d power" % (self.name, self.health, self.power))
+        
+class Mewtwo(Pokemon):
+    def print_status(self):
+        print("%s has %d health and %d power" % (self.name, self.health, self.power))
 
 def pokemon_selector():
    # Change the class names accordingly
     pokemon_list = {
         #Character name, health, basic attack, seconday attack, special:
         #Possibly add another for coins/loot to give?
-        1 : Charizard("Charizard", 100, 50, 140 ,110),
+        1 : Charizard("Charizard", 100, 50, 140, 110),
         2: Squirtle("Squirtle", 100, 40, 60, 110),
         3: Bulbasaur("Bulbasaur", 100, 45, 55, 120),
         4: Gyarados("Gyarados", 100, 40, 90, 140),
@@ -49,7 +77,6 @@ def pokemon_selector():
         for pokemon in pokemon_list:
             pokemon_checker = pokemon_list[pokemon].name
             #Checks to make sure the selector is working correctly
-            print(pokemon_checker)
         
         while opponent is None:
             if player.name != pokemon_checker:
@@ -64,6 +91,17 @@ def pokemon_selector():
         #Future: print the opponent's image here(?)
                 
         print("A wild %s has appeared!" % (opponent.name))
+        #Checks player selection - delete later:
+        #print(player.__dict__)
+        print("""%s is ready! Your pokemon's stats are:
+              
+              Health: %d
+              Basic attack: %d
+              Seconday Attack: %d
+              Special Attack: %d
+              
+              """
+              % (player.name, player.health, player.basic_attack, player.secondary_attack, player.special_attack))
         return player, opponent
     
 pokemon_selector()
