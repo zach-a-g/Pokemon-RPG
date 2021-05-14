64848bfff4198b99d751ffded7c3383cb627074b
Zach Gleeson <zachary.a.gleeson@gmail.com>
2021-05-14 11:53:46 -0500

    Changed code to include battle

--- Pokemon-RPG.py
+++ Pokemon-RPG.py
@@ -1,24 +1,27 @@
 
 import random
-from Items import Item, Crit_Potion, Pokeball, Master_Pokeball, Health
+from items import Item, Crit_Potion, Pokeball, Master_Pokeball, Health
 from random import randint
+from pokemon_art import bulbasaur_art, charmander_art, squirtle_art
+import os
+import time
 
 # POKEMON CLASSES
 
 #Basic class and subclasses for testing purposes
 
 class Pokemon:
-    def __init__(self, name, health, basic_attack, secondary_attack, special_attack):
+    def __init__(self, name, health, attack):
         self.name = name
         self.health = health
         self.bounty = 0
         self.potions = 1
-        self.basic_attack = basic_attack
-        self.secondary_attack = secondary_attack
-        self.special_attack = special_attack
+        self.attack = attack
+        # self.secondary_attack = secondary_attack
+        # self.special_attack = special_attack
         
 
-class Charizard(Pokemon):
+class Charmander(Pokemon):
     def print_status(self):
         print("%s has %d health and %d power" % (self.name, self.health, self.power))
 
@@ -41,29 +44,7 @@ class Mewtwo(Pokemon):
         print("%s has %d health and %d power" % (self.name, self.health, self.power))
 
 
-class Pokemon:
-    def __init__(self, name, health, basic_attack, secondary_attack, special_attack):
-        self.name = name
-        self.health = health
-        self.bounty = 0
-        self.potions = 1
-        self.basic_attack = basic_attack
-        self.secondary_attack = secondary_attack
-        self.special_attack = special_attack
-        
 
-class Charmander(Pokemon):
-    def print_status(self):
-        print("%s has %d health and %d power" % (self.name, self.health, self.power))
-
-
-class Squirtle(Pokemon):
-    def print_status(self):
-        print("%s has %d health and %d power" % (self.name, self.health, self.power))
-
-class Bulbasaur(Pokemon):
-    def print_status(self):
-        print("%s has %d health and %d power" % (self.name, self.health, self.power))
 
 
 def menu_launch():
@@ -79,9 +60,9 @@ def menu_launch():
 """)
     
     starter_pokemon = {
-    1: Charmander("Charmander", 100, 50, 45, 110),
-    2: Squirtle("Squirtle", 100, 40, 60, 110),
-    3: Bulbasaur("Bulbasaur", 100, 45, 55, 120),
+    1: Charmander("Charmander", 100, 50),
+    2: Squirtle("Squirtle", 100, 40),
+    3: Bulbasaur("Bulbasaur", 100, 45),
     }
     
     user_name = input("What is your name young trainer? ")
@@ -104,34 +85,147 @@ def menu_launch():
         pokemon_choice = int(input("Who do you choose? "))
         if pokemon_choice == 1:
             player = starter_pokemon[1]
+            charmander_art()
         elif pokemon_choice == 2:
             player = starter_pokemon[2]
+            squirtle_art()
         elif pokemon_choice == 3:
             player = starter_pokemon[3]
+            bulbasaur_art()
     
         else:
             print("Please choose a number 1 - 3.")
     
-        print(player.name)
-        
         print("What an excellent choice!! Take care of %s for us!" % (player.name))
         #Tests the selection above - delete later
-        print(player.__dict__)
+        #print(player.__dict__)
         running = False
         main(player)
 
 
 
 def main(player):
-    print("What do you and %s want to do? " % (player.name))
-    shop(player)
+    print("""
+          1. Find wild pokemon
+          2. Visit Nurse Joy
+          3. Visit the Store
+    
+          """)
+    main_input = int(input("What do you and %s want to do? " % (player.name)))
+    if main_input == 1:  
+        battle(player)
+    elif main_input == 2:
+        print("testing")
+        # medic(player)
+    elif main_input == 3:
+        shop(player)
+    else:
+        print("test")
+
+
+def battle(player):
+
+    charizard = Pokemon("Charizard", 100, 20)
+    blastoise = Pokemon("Blastoise",100, 20)
+    mewtwo = Pokemon('Mewtwo', 100, 25)
+    squirtle = Pokemon('Squirtle', 100, 25)
+
+    characters = [" ", charizard, blastoise, mewtwo, squirtle]
+
+    opponent_list = [charizard, blastoise, mewtwo, squirtle]
+    opponent = random.choice(opponent_list)
+    battle = True
+    # opponent_list = {
+    #     1: Gyarados("Gyarados", 100, 50, 45, 110),
+    #     2: Mewtwo("Mewtwo", 200, 70, 80, 200 ),
+    #     3: Charmander("Charmander", 100, 50, 45, 110),
+    #     4: Squirtle("Squirtle", 100, 40, 60, 110),
+    #     5: Bulbasaur("Bulbasaur", 100, 45, 55, 120),
+    # }
+    while battle:
+        # random_number = randint(1, 5)
+        # opponent = opponent_list[random_number]
+        print("A wild %s appears!" % (opponent.name)) 
+        print(player.name)
+        #Launch into battle sequence here
+        os.system('clear')
+        # print(logo)
+        action = input("""What would you like to do?
+            1.Attack
+            2.Defend
+            3.Use an item
+            4.Flee
+            """)
+        if action == '1' and player.health > 0:
+            time.sleep(3)
+            print(player.health)
+            print("%s attacked %s." % (player.name, opponent.name))
+            time.sleep(3)
+            print(' ')
+            opponent.health -= player.attack 
+            print("%s's health has decreased to %d." % (opponent.name, opponent.health))
+            time.sleep(3)
+            #Add statement to make value 0 to avoid negative health
+            if opponent.health <= 0:
+                #add delay print later
+                print("%s has fainted!" % opponent.name)
+                # print("%s dropped %d coins!" % opponent.name, random.randrange(20, 100))
+                break
+            else:
+                pass
+            
+            if opponent.health > 0:
+                print("""============================================================""")
+                print("It's your enemy's turn to attack.")
+                time.sleep(1)
+                player.health -= opponent.attack
+                print("""%s's health is now %d""" % (player.name, opponent.health))
+                time.sleep(4)
+            
+            else:
+                print("Please enter a valid option.")
+        
+        elif action == '2':
+            random.randrange(1, 4)
+            if random.random() <= 0.5:
+                print("%s defended itself and took no damage." % (player.name))
+                time.sleep(3)
+
+            else:
+                player.health -= opponent.attack
+                print("%s defended itself, but it failed." % (player.name)) 
+                print("%s health is: %d" % (player.name, player.health))
+                time.sleep(3)
+
+        else:
+            pass
+
+    
+    
+# def defend_sequence(player, opponent):
+#         if player.health > 0:
+#                 player.health -= opponent.attack / 2
+#                 print("%s defended itself. Damage was reduced to half. %s's health is now %d" % (player.name, player.name, player.health))
 
 def shop(player):
-    print("This is a test %s" % (player.name))
+    print("Welcome to the shop! What can we help you with today?")
+    print("""
+          1. Purchase Potions 
+          2. Purchase attack items
+          3. Purchase Pokeballs
+          """)
+    shop_input = int(input(""))
+    if shop_input == 1:
+        player.bounty -= 50
+        player.potions += 1
+        print("Thanks for your purchase!")
+    elif shop_input == 2:
+        player.bounty -= 25
+        player.basic_ttacks += 25
+        print("Thanks for your purchase!")
+    else:
+        print("Thanks for stopping by!")
+    main(player)
 
 
 menu_launch()
-
-
-
-
