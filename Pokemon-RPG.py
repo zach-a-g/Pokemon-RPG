643b3b3a33d78c932017b85d144126f5b3026094
Zach Gleeson <zachary.a.gleeson@gmail.com>
2021-05-13 19:28:46 -0500

    Updated main to work off of

--- Pokemon-RPG.py
+++ Pokemon-RPG.py
@@ -1,77 +1,139 @@
 
+import os
+import time
 import random
 from Items import Item, Crit_Potion, Pokeball, Master_Pokeball, Health
+from random import randint
 
 # POKEMON CLASSES
 
+#Basic class and subclasses for testing purposes
+
 class Pokemon:
-    def __init__(self, name, health, attack, defense):
+    def __init__(self, name, health, basic_attack, secondary_attack, special_attack):
         self.name = name
         self.health = health
-        self.attack = attack
-        self.defense = defense
-        self.potions = 3
-
-    def attack_opponent(self, other_pokemon):
-        # While loop that starts the battle
-        print("A WILD %s APPEARS!" % other_pokemon)
-        while self.health > 0 or other_pokemon.health == 0:
-            desire_to_attack = input("Choose your attack: List of Commands")
-            lower_desire_to_attack = desire_to_attack.lower()
-            #If the user wants to attack do:
-            if lower_desire_to_attack == "yes":
-                other_pokemon.health = other_pokemon.health - (self.attack * 0.1)
-                self.attack -= 5
-                self.health = self.health - (other_pokemon.attack * 0.1)
-                print("%s's health has decreased to %d" % (other_pokemon.name, other_pokemon.health))
-                print("-----------")
-                print("%s's attack has decreased to %d" % (self.name, self.attack))
-                print("%s attacked your pokemon. Your health fell to: %d" % (other_pokemon.name, self.health))
-            # If the user does not want to attack:
-            elif lower_desire_to_attack == "no":
-                print("""
-                1. Flee
-                2. Take Potion
-                    """)
-                flee_or_potion = int(input("What do you want to do? Type 1 or 2." ))
-                # Option for taking potions
-                if flee_or_potion == 2:
-                    if self.potions > 0:
-                        self.health += 20
-                        self.potions -= 1
-                        print("Your pokemon's health is now at %d. You have %d potions left" % (self.health, self.potions))
-                    else:
-                        print("You don't have any more potions.")
-                 # Option for fleeing the battle   
-                else:
-                    print("You have fled the battle.")
-                    self.health = 0
-            else:
-                print("Please enter a valid option.")
-                 
-    
-    def list_stats(self):
-        print("""%s's stats:
-              Health: %d
-              Attack: %d
-              Defense: %d
-              """ % (self.name, self.health, self.attack, self.defense))
+        self.bounty = 0
+        self.potions = 1
+        self.basic_attack = basic_attack
+        self.secondary_attack = secondary_attack
+        self.special_attack = special_attack
         
-running = True
 
-charizard = Pokemon("Charizard", 100, 120, 80)
-blastoise = Pokemon("Blastoise",100, 100, 110)
-
-charizard.attack_opponent(blastoise)
+class Charizard(Pokemon):
+    def print_status(self):
+        print("%s has %d health and %d power" % (self.name, self.health, self.power))
 
 
+class Squirtle(Pokemon):
+    def print_status(self):
+        print("%s has %d health and %d power" % (self.name, self.health, self.power))
 
-# print("Welcome to Pokemon!")
+class Bulbasaur(Pokemon):
+    def print_status(self):
+        print("%s has %d health and %d power" % (self.name, self.health, self.power))
+        
 
-# while running:
-#     print("""
-#           1. View Pokemon's stats
-#           2. Fight another pokemon 
-#           3. Exit
-#           """)
-#     user_input = int(input("What would you like to do? Select a number 1-3"))
+class Gyarados(Pokemon):
+    def print_status(self):
+        print("%s has %d health and %d power" % (self.name, self.health, self.power))
+        
+class Mewtwo(Pokemon):
+    def print_status(self):
+        print("%s has %d health and %d power" % (self.name, self.health, self.power))
+
+
+class Pokemon:
+    def __init__(self, name, health, basic_attack, secondary_attack, special_attack):
+        self.name = name
+        self.health = health
+        self.bounty = 0
+        self.potions = 1
+        self.basic_attack = basic_attack
+        self.secondary_attack = secondary_attack
+        self.special_attack = special_attack
+        
+
+class Charmander(Pokemon):
+    def print_status(self):
+        print("%s has %d health and %d power" % (self.name, self.health, self.power))
+
+
+class Squirtle(Pokemon):
+    def print_status(self):
+        print("%s has %d health and %d power" % (self.name, self.health, self.power))
+
+class Bulbasaur(Pokemon):
+    def print_status(self):
+        print("%s has %d health and %d power" % (self.name, self.health, self.power))
+
+
+def menu_launch():
+    print("""
+================================================
+  _____   ____  _  ________ __  __  ____  _   _ 
+ |  __ \ / __ \| |/ /  ____|  \/  |/ __ \| \ | |
+ | |__) | |  | | ' /| |__  | \  / | |  | |  \| |
+ |  ___/| |  | |  < |  __| | |\/| | |  | | . ` |
+ | |    | |__| | . \| |____| |  | | |__| | |\  |
+ |_|     \____/|_|\_\______|_|  |_|\____/|_| \_|
+================================================
+""")
+    
+    starter_pokemon = {
+    1: Charmander("Charmander", 100, 50, 45, 110),
+    2: Squirtle("Squirtle", 100, 40, 60, 110),
+    3: Bulbasaur("Bulbasaur", 100, 45, 55, 120),
+    }
+    
+    user_name = input("What is your name young trainer? ")
+    print("Nice to meet you %s. I'm Professor Oak. I'll be helping you on your quest to become a pokemon trainer. Let's get you set up with a pokemon. Choose wisely, this pokemon will become your best friend and trusted ally." % (user_name))
+    print("")
+    print("====================================================================================")
+    print("")
+    print("Choose your pokemon:")
+    print("""
+    1. Charmander
+    2. Squirtle
+    3. Bulbasaur
+              """)
+    # user_input = int(input(""))
+    # print(user_input)
+    
+    player = None
+    running = True
+    while running:
+        pokemon_choice = int(input("Who do you choose? "))
+        if pokemon_choice == 1:
+            player = starter_pokemon[1]
+        elif pokemon_choice == 2:
+            player = starter_pokemon[2]
+        elif pokemon_choice == 3:
+            player = starter_pokemon[3]
+    
+        else:
+            print("Please choose a number 1 - 3.")
+    
+        print(player.name)
+        
+        print("What an excellent choice!! Take care of %s for us!" % (player.name))
+        #Tests the selection above - delete later
+        print(player.__dict__)
+        running = False
+        main(player)
+
+
+
+def main(player):
+    print("What do you and %s want to do? " % (player.name))
+    shop(player)
+
+def shop(player):
+    print("This is a test %s" % (player.name))
+
+
+menu_launch()
+
+
+
+
